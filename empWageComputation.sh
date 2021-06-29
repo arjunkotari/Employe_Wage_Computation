ce9c9e7dc77d1475691dad43c519850253ae7572
arjunkotari <arjun.kotari123@gmail.com>
2021-06-29 13:31:31 +0530

    Add Part time Employee & Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,15 +1,22 @@
 echo "Welcome to Employee Wage Computation"
-present=1
+fullTime=1
+partTime=2
 perHr=20
 fullDayHrs=0
-if [ $((RANDOM%2)) -eq 1 ]
+if [ $((RANDOM%3)) -eq 1 ]
 then
-	echo "Employee present"
+	echo "Employee present full time"
 	fullDayHrs=8
 	total=$(($perHr*$fullDayHrs))
 	echo "Daily Employee Wage is $total"
+elif [ $((RANDOM%3)) -eq 2 ]
+then
+	echo "Employye present part time"
+	fullDayHrs=4
+	total=$(($perHr*$fullDayHrs))
+        echo "Daily Employee Wage is $total"
 else
-	echo "Employye absent"
+	echo "Employee Absent" 
 	total=$(($perHr*$fullDayHrs))
         echo "Daily Employee Wage is $total"
 fi
