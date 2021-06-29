19ece80bd8e59b1257b444aaae6eab3de956c31c
arjunkotari <arjun.kotari123@gmail.com>
2021-06-29 13:14:26 +0530

    Checking Employee Daily Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,8 +1,15 @@
 echo "Welcome to Employee Wage Computation"
 present=1
-if [ $(($RANDOM%2)) -eq 1 ]
+perHr=20
+fullDayHrs=0
+if [ $((RANDOM%2)) -eq 1 ]
 then
 	echo "Employee present"
+	fullDayHrs=8
+	total=$(($perHr*$fullDayHrs))
+	echo "Daily Employee Wage is $total"
 else
 	echo "Employye absent"
+	total=$(($perHr*$fullDayHrs))
+        echo "Daily Employee Wage is $total"
 fi
